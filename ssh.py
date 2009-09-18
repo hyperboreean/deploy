2ca4c2097c9e51659a1925312bf88adf997486bb
hyperboreean <hyperboreean@nerdshack.com>
2009-09-18 21:24:36 +0300

    Dorking around.
    Signed-off-by: hyperboreean <hyperboreean@nerdshack.com>

--- ssh.py
+++ ssh.py
@@ -1,9 +1,17 @@
-import paramiko
+try:
+    import paramiko
+except ImportError:
+    import sys
+    print >> sys.stderr 'paramiko library required.'
+    sys.exit(1)
 
 
 __all__ = ['SSH']
 
 
 class SSH(object):
-    def __init__(self):
-        pass
+    def __init__(self, server, user, password = None):
+        self._server = server
+        self._user = user
+        self._password = password
+
